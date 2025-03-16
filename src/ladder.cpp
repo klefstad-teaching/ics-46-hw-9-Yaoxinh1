056de903212b09c91fce422bb50c2c01c0bf4d4a
Yaoxin <yaoxinh@uci.edu>
2025-03-16 01:44:36 +0000

    hw9

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -175,16 +175,14 @@ void print_word_ladder(const vector<string>& ladder) {
     cout << "\n";
 }
 
-// 7) verify_word_ladder()
+
 void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "words.txt");
 
+    auto ladder1 = generate_word_ladder("apple", "apple", word_list);
+    print_word_ladder(ladder1);
 
-    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
-    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
-    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
-    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
-    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
-    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+    auto ladder2 = generate_word_ladder("cat", "dog", word_list);
+    print_word_ladder(ladder2);
 }
